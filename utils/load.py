386fc0f149dd32179b34e01a6d9e8135ed38b991
thiliapr <140580293+thiliapr@users.noreply.github.com>
2024-08-10 11:36:47 +0800

    load dicts with no raise

--- utils/load.py
+++ utils/load.py
@@ -2,12 +2,13 @@
 # 本文件是 TkTransl 的一部分。
 # TkTransl 是自由软件：你可以再分发之和/或依照由自由软件基金会发布的 GNU 通用公共许可证修改之，无论是版本 3 许可证，还是（按你的决定）任何以后版都可以。
 # 发布 TkTransl 是希望它能有用，但是并无保障;甚至连可销售和符合某个特定的目的都不保证。请参看 GNU 通用公共许可证，了解详情。
-# 你应该随程序获得一份 GNU 通用公共许可证的复本。如果没有，请看 <https://www.gnu.org/licenses/>。 
+# 你应该随程序获得一份 GNU 通用公共许可证的复本。如果没有，请看 <https://www.gnu.org/licenses/>。
 
 import json
 from pathlib import Path
 from typing import Optional
 from utils.translate import Message
+from utils.extra import log, LogLevel
 
 
 def load_dicts(paths: tuple[list[Path], list[Path], list[Path]]) -> tuple[list[tuple[str, str]], list[tuple[str, str]], list[tuple[str, str, Optional[str]]]]:
@@ -19,13 +20,13 @@ def load_dicts(paths: tuple[list[Path], list[Path], list[Path]]) -> tuple[list[t
             list[Path]: 译前词典。
             list[Path]: 译后词典。
             list[Path]: GPT词典。
-    
+
     Returns:
         list[tuple[原文, 译文]]: 译前词典。
         list[tuple[原文, 译文]]: 译后词典。
         list[tuple[原文, 译文, Optional[额外信息]]]: GPT词典。
     """
-    
+
     # 读取词典
     dicts: tuple[list[tuple[str, str]], list[tuple[str, str]], list[tuple[str, str, Optional[str]]]] = [], [], []
     for index, dict_paths in enumerate(paths):
@@ -39,7 +40,8 @@ def load_dicts(paths: tuple[list[Path], list[Path], list[Path]]) -> tuple[list[t
                     if not entry_str:
                         continue
                     elif "->" not in entry_str:
-                        raise Exception(f"Entry missing \"->\": {entry_str}")
+                        log("load_messages()", f"词条中缺少了`->`, 将会跳过该词条: {entry_str}", LogLevel.Warning)
+                        continue
 
                     # 分割原文、译文等
                     src, other = entry_str.split("->", 1)
@@ -50,13 +52,13 @@ def load_dicts(paths: tuple[list[Path], list[Path], list[Path]]) -> tuple[list[t
                         dest, info = other[0], other[1] if len(other) > 1 else None
                     else:
                         dest = other
-                    
+
                     # 添加词条至词典
                     if index == 2:
                         dicts[index].append((src, dest, info))
                     else:
-                        dicts[index].append((src, dest)) #type: ignore
-    
+                        dicts[index].append((src, dest))  # type: ignore
+
     return dicts
 
 
@@ -86,7 +88,7 @@ def load_messages(input_path: Path, output_path: Path) -> dict[str, list[Message
                 for index, message in enumerate(json.load(f))
                 if index not in messages_translated
             ]
-        
+
         # 滤除不需要翻译的文件
         if file_messages:
             messages[str(message_path)] = file_messages
