a2e78f4ffcf7536921dfc9c37e900d054a6a8f50
thiliapr <140580293+thiliapr@users.noreply.github.com>
2025-05-24 09:24:26 +0800

    联系方式

--- sakurallm.py
+++ sakurallm.py
@@ -1,11 +1,10 @@
 "SakuraLLM的翻译部分"
 # Copyright (C) 2025  thiliapr
-# 本文件是 TkTransl 的一部分。
-# TkTransl 是自由软件: 你可以再分发之和/或依照由自由软件基金会发布的 GNU 通用公共许可证修改之，
-# 无论是版本 3 许可证，还是（按你的决定）任何以后版都可以。
-# 发布 TkTransl 是希望它能有用，但是并无保障; 甚至连可销售和符合某个特定的目的都不保证。
-# 请参看 GNU 通用公共许可证，了解详情。
-# 你应该随程序获得一份 GNU 通用公共许可证的复本。如果没有，请看 <https://www.gnu.org/licenses/>。
+# Email: thiliapr@tutanota.com
+# 本文件是 tktransl 的一部分。
+# tktransl 是自由软件：你可以再分发之和/或依照由自由软件基金会发布的 GNU Affero 通用公共许可证修改之，无论是版本 3 许可证，还是（按你的决定）任何以后版都可以。
+# 发布 tktransl 是希望它能有用，但是并无保障；甚至连可销售和符合某个特定的目的都不保证。请参看 GNU Affero 通用公共许可证，了解详情。
+# 你应该随程序获得一份 GNU Affero 通用公共许可证的复本。如果没有，请看 <https://www.gnu.org/licenses/>。
 
 import json
 import httpx
